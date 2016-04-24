8d98086074bf8cdd9d91ab7743fd733abecfe320
Colby Ford <colbytylerford@gmail.com>
2016-04-24 19:39:46 -0400

    VCF output Fix

--- ACCTool/settings.py
+++ ACCTool/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'analysis/uploads')
 MEDIA_URL = 'analysis/uploads/'
 
 
