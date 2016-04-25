c7bb60c1eda11ff0686dc36d42a5cbaee11d0a4a
Colby Ford <colbytylerford@gmail.com>
2016-04-25 19:20:28 -0400

    Remove dj_settings

--- ACCTool/settings.py
+++ ACCTool/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,18 +124,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'analysis/uploads')
 MEDIA_URL = 'analysis/uploads/'
-
-
-##Heroku Stuff
-DATABASES['default'] = dj_database_url.config()
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-ALLOWED_HOSTS = ['*']
-
-DEBUG = False
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
